e1f34bfdbc0968cef441b888d4b71f67f0e41bb9
MilkyCode13 <shmayhel.andrey@gmail.com>
2023-07-07 13:01:06 +0300

    Create empty project

---
+++ src/main/java/ru/cringules/moodtool/backend/MoodToolBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.cringules.moodtool.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodToolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodToolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cringules/moodtool/backend/MoodToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cringules.moodtool.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodToolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
